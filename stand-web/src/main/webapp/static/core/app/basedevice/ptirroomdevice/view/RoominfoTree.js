02545e09be3133e0630bde93fa205a0ea90b186f
Tzy <1228500587@qq.com>
2017-10-19 17:09:47 +0800

    红外设备品牌树单击事件

--- stand-web/src/main/webapp/static/core/app/basedevice/ptirroomdevice/view/RoominfoTree.js
+++ stand-web/src/main/webapp/static/core/app/basedevice/ptirroomdevice/view/RoominfoTree.js
@@ -46,67 +46,6 @@ Ext.define("core.basedevice.ptirroomdevice.view.RoominfoTree", {
     },
     listeners: {
         itemclick: function (tree, record, item, index, e, eOpts) {
-//            var mainLayout = tree.up("panel[xtype=basedevice.ptirroomdevice.mainlayout]");
-//            var areaId = record.get("id");
-//            var level = record.get("level");
-//            var filter = [];
-//            var roomIds = new Array();
-//            
-//            filter.push({"type":"string","comparison":"=","value": areaId ,"field":"areaId"});
-//            var funData = mainLayout.funData;
-//            mainLayout.funData = Ext.apply(funData, {
-//                areaId: record.get("id"),
-//                areaName: record.get("text"),
-//                level:record.get("level"),
-//                roomIds:roomIds.join(","),
-//                filter: JSON.stringify(filter),
-//            });
-//            // 加载对应的房间信息
-//            var roomGrid = mainLayout.down("panel[xtype=basedevice.ptirroomdevice.maingrid]");
-//            var store = roomGrid.getStore();
-//            var proxy = store.getProxy();
-//
-//            if(level==5){
-//                    roomIds.push(record.id);
-//            }
-//            
-//            if(level==4){
-//            	var childrens = record.childNodes;
-//                Ext.each(childrens, function (rec) {
-//                    var roomId = rec.id;
-//                    roomIds.push(roomId);
-//                });
-//            }
-//            
-//            if(level==3){
-//            	var childrens = record.childNodes;
-//                for(var i=0;i<childrens.length;i++){
-//                	Ext.each(childrens[i].childNodes, function (rec) {
-//                        var roomId = rec.id;
-//                        roomIds.push(roomId);
-//                    });
-//                }
-//            }
-//            
-//            proxy.extraParams.filter = JSON.stringify(filter);
-//            proxy.extraParams.areaId= areaId;
-//            proxy.extraParams.level= level;
-//            proxy.extraParams.roomIds= roomIds.join(",");
-//            store.loadPage(1); // 给form赋值
-//            return false;
-    
-//        itemclick: function(view, record, item, index, e) {
-//            var mainLayout = view.up("panel[xtype=basedevice.ptirroomdevice.mainlayout]");
-//            var storeyGrid = mainLayout.down("panel[xtype=basedevice.ptirroomdevice.maingrid]");
-//            var store = storeyGrid.getStore();
-//            var proxy = store.getProxy();
-//            proxy.extraParams = {
-//                filter: "[{'type':'string','comparison':'=','value':'" + record.get('id') + "','field':'roomId'}]"
-//            };
-//            store.load(); // 给form赋值
-//            return false;
-//        }
-//    }
         	var mainLayout = tree.up("panel[xtype=basedevice.ptirroomdevice.mainlayout]");
         	var basetreegrid = mainLayout.down("basetreegrid[xtype=basedevice.ptirroomdevice.roominfotree]");
         	var funData = mainLayout.funData;
