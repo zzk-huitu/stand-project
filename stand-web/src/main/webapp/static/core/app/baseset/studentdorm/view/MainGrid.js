86e3c778b3a191a04be3ee058b9fb3d74949909b
xioaxie0507 <1625851920@qq.com>
2017-12-06 17:14:09 +0800

    update

--- stand-web/src/main/webapp/static/core/app/baseset/studentdorm/view/MainGrid.js
+++ stand-web/src/main/webapp/static/core/app/baseset/studentdorm/view/MainGrid.js
@@ -40,7 +40,7 @@ Ext.define("core.baseset.studentdorm.view.MainGrid", {
             iconCls: 'x-fa fa-plus-circle',
         }, {
             xtype: 'button',
-            text: '导出Excel',
+            text: '导出',
             ref: 'exportExcel',
             funCode:'girdFuntionBtn',   //指定此类按钮为girdFuntionBtn类型，用于于右边的按钮进行功能区分
             iconCls: 'x-fa fa-file',
