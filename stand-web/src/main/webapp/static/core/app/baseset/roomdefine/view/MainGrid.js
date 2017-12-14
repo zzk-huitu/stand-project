be8626ac01db0a3e4fca662953997c3bd7b24584
xioaxie0507 <1625851920@qq.com>
2017-12-14 17:52:50 +0800

    update

--- stand-web/src/main/webapp/static/core/app/baseset/roomdefine/view/MainGrid.js
+++ stand-web/src/main/webapp/static/core/app/baseset/roomdefine/view/MainGrid.js
@@ -118,19 +118,19 @@ Ext.define("core.baseset.roomdefine.view.MainGrid", {
                 text:'编辑宿舍',  
                 style:'font-size:12px;', 
                 tooltip: '编辑宿舍',
-                ref: 'gridEdit_Tab',
+                ref: 'gridEdit',
                 getClass :function(v,metadata,record,rowIndex,colIndex,store){   
                     if(record.get("roomType")!="1"){                    
                         return 'x-hidden-display';                    
-                    }/*else{
+                    }else{
                        if(comm.get("isAdmin")!="1"){
                         var menuCode="BASEROOMDEFINE";     // 此菜单的前缀
                         var userBtn=comm.get("userBtn");                 
-                        if(userBtn.indexOf(menuCode+"_gridEdit_Tab")==-1){
+                        if(userBtn.indexOf(menuCode+"_gridEdit")==-1){
                             return 'x-hidden-display';
                         }
                     } 
-                }*/
+                }
                     return null; 
                 }, 
                 handler: function(view, rowIndex, colIndex, item) {
